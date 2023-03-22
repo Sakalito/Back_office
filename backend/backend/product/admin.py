55f6ca270b8c8a13b5057e7c18603e548253c473
Lbrzr <akakpoleandre0@gmail.com>
2023-03-22 15:06:44 +0100

    product, product owner, category admin models updated

--- backend/backend/product/admin.py
+++ backend/backend/product/admin.py
@@ -4,6 +4,26 @@ from .models import ProduitModel, ProductOwnerModel, CategoryModel
 
 # Register your models here.
 
-admin.site.register(ProduitModel)
-admin.site.register(ProductOwnerModel)
-admin.site.register(CategoryModel)
+
+class ProduitAdminModel(admin.ModelAdmin):
+    list_display = ('id', 'name', 'price', 'category', 'owner',)
+    list_filter = ('category', 'owner')
+    search_fields = ('name', 'price', 'category', 'owner')
+    list_per_page = 25
+
+
+class ProductOwnerAdminModel(admin.ModelAdmin):
+    list_display = ('id', 'name',)
+    search_fields = ('name',)
+    list_per_page = 25
+
+
+class CategoryAdminModel(admin.ModelAdmin):
+    list_display = ('id', 'name',)
+    search_fields = ('name',)
+    list_per_page = 25
+
+
+admin.site.register(ProduitModel, ProduitAdminModel)
+admin.site.register(ProductOwnerModel, ProductOwnerAdminModel)
+admin.site.register(CategoryModel, CategoryAdminModel)
