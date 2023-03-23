836c8dc17e5260159d4f4e7185c632ba371c9298
Lbrzr <akakpoleandre0@gmail.com>
2023-03-23 15:03:22 +0100

    product man: update and partial update tested, jwt token lifetime increased

--- backend/backend/backend/settings.py
+++ backend/backend/backend/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+from datetime import timedelta
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -137,4 +138,8 @@ REST_FRAMEWORK = {
         # 'rest_framework.authentication.SessionAuthentication',
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ]
+}
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=10),
 }
--- backend/backend/product/views.py
+++ backend/backend/product/views.py
@@ -30,6 +30,9 @@ class ProductDetailView(generics.GenericAPIView, mixins.RetrieveModelMixin, mixi
 
     def put(self, request, *args, **kwargs):
         return self.update(request, *args, **kwargs)
+    
+    def patch(self, request, *args, **kwargs):
+        return self.partial_update(request, *args, **kwargs)
 
     def delete(self, request, *args, **kwargs):
         return self.destroy(request, *args, **kwargs)
